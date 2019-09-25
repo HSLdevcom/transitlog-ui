7df5ad12b42b041ec5fd87869b203e8fde660a64
Daniel Dunderfelt <daniel@developsuperpowers.com>
2019-09-25 06:51:28 +0300

    Run prettier.

--- src/helpers/createEntityId.js
+++ src/helpers/createEntityId.js
@@ -7,23 +7,15 @@ export function createEntityId(obj) {
         return `vehicle_option_${obj.unique_vehicle_id}`;
       }
 
-      return `${obj.__typename}_${obj.journey_start_time}_${obj.route_id}_${
-        obj.direction_id
-      }_${obj.received_at}_${obj.lat}_${obj.long}_${obj.unique_vehicle_id}`;
+      return `${obj.__typename}_${obj.journey_start_time}_${obj.route_id}_${obj.direction_id}_${obj.received_at}_${obj.lat}_${obj.long}_${obj.unique_vehicle_id}`;
     case "Line":
       return `${obj.__typename}_${obj.lineId}_${obj.dateBegin}_${obj.dateEnd}`;
     case "Route":
-      return `${obj.__typename}_${obj.routeId}_${obj.direction}_${obj.dateBegin}_${
-        obj.dateEnd
-      }`;
+      return `${obj.__typename}_${obj.routeId}_${obj.direction}_${obj.dateBegin}_${obj.dateEnd}`;
     case "RouteSegment":
-      return `${obj.__typename}_${obj.routeId}_${obj.direction}_${obj.dateBegin}_${
-        obj.dateEnd
-      }_${obj.stop}`;
+      return `${obj.__typename}_${obj.routeId}_${obj.direction}_${obj.dateBegin}_${obj.dateEnd}_${obj.stop}`;
     case "Departure":
-      return `${obj.__typename}_${obj.routeId}_${obj.direction}_${obj.dateBegin}_${
-        obj.dateEnd
-      }_${obj.hours}_${obj.minutes}_${obj.stopId}_${obj.dayType}`;
+      return `${obj.__typename}_${obj.routeId}_${obj.direction}_${obj.dateBegin}_${obj.dateEnd}_${obj.hours}_${obj.minutes}_${obj.stopId}_${obj.dayType}`;
     default:
       console.log(obj.__typename);
       return;
