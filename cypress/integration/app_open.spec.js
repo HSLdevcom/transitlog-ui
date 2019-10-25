90ab5081f4fc2a5638f3d67cb8df9d2d6b58a664
Daniel Dunderfelt <daniel@developsuperpowers.com>
2019-10-25 08:29:21 +0300

    Add command for selecting testid and move test up.

--- cypress/integration/app_open.spec.js
+++ cypress/integration/app_open.spec.js
@@ -9,10 +9,9 @@ describe("App opening smoke tests", () => {
   it("Opens the app", () => {
     cy.contains("Reittiloki");
 
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
     cy.wait(5000);
 
-    cy.window().then((win) => {
-      expect(win.console.error).to.have.callCount(0);
-    });
+    expect(cy.get("spyWinConsoleError")).to.have.callCount(0);
   });
 });
