249d013f60db72368111b29cefc5f2343c2b20db
Daniel Dunderfelt <daniel@developsuperpowers.com>
2019-10-29 13:17:24 +0200

    Check auth cookie in test.

--- cypress/integration/authentication.spec.js
+++ cypress/integration/authentication.spec.js
@@ -6,7 +6,11 @@ describe("Authentication smoke tests", () => {
   });
 
   it("Can log in with HSL ID", () => {
+    cy.getCookie("transitlog-session").should("not.exist");
+
     cy.hslLogin();
     cy.getTestElement("authenticated-user", {timeout: 10000}).should("exist");
+
+    cy.getCookie("transitlog-session").should("exist");
   });
 });
