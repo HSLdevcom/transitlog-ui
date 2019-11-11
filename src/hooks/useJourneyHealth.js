9f4b45a07048b74427ba0de61b9d8eb106a53431
Daniel Dunderfelt <daniel@developsuperpowers.com>
2019-11-11 07:46:52 +0200

    Adjust health colors and give more penalties for missing stop events.

--- src/hooks/useJourneyHealth.js
+++ src/hooks/useJourneyHealth.js
@@ -129,6 +129,7 @@ function checkStopEventsHealth(stopEvents, plannedStops, incrementHealth, addMes
       const presentEvents = eventsForStop.map((evt) => evt.type);
       const missingEvents = difference(stopEventTypes, presentEvents);
       addMessage(`Events missing for stop ${stopId}: ${missingEvents.join(", ")}`);
+      incrementHealth(-(missingEvents.length * 10));
     }
   }
 }
@@ -139,6 +140,7 @@ export const useJourneyHealth = (journey) => {
     const vehiclePositions = get(journey, "vehiclePositions", []);
     const plannedDepartures = get(journey, "routeDepartures", []);
 
+    // Ensure we have all required data. Bail here is not.
     if (
       !journey ||
       plannedDepartures.length === 0 ||
@@ -147,6 +149,7 @@ export const useJourneyHealth = (journey) => {
       return null;
     }
 
+    // Separate events into stop events and non-stop events.
     const {stopEvents, events} = journeyEvents.reduce(
       (categories, event) => {
         if (stopEventTypes.includes(event.type)) {
@@ -160,38 +163,55 @@ export const useJourneyHealth = (journey) => {
       {stopEvents: [], events: []}
     );
 
-    const checkStopsFrom = vehiclePositions.length !== 0 ? vehiclePositions : stopEvents;
-    const lastEventWithStop = findLast(checkStopsFrom, (pos) =>
-      pos.stop ? !!pos.stop : !!pos.stopId
-    );
+    // Get the furthest stop that we have events from. For journeys that are in
+    // progress, the data is validated up to here. In other words we don't expect
+    // events to be present from stops that are in the future of the journey. The
+    // furthest driven stop is checked from the vehicle positions to disentangle
+    // this check from the data that we are actually checking; the stop events.
+    const lastEventWithStop = findLast(vehiclePositions, (pos) => !!pos.stop);
+    const maxDrivenStop = get(lastEventWithStop, "stop", "");
 
-    const maxDrivenStop = get(
-      lastEventWithStop,
-      "stop",
-      get(lastEventWithStop, "stopId", "")
-    );
-
+    // The index of the max stop.
     const stopsVisitedCount = plannedDepartures.findIndex(
       (dep) => dep.stopId === maxDrivenStop
     );
 
+    // This is the destination stop of the journey.
     const lastPlannedStop = get(last(plannedDepartures), "stopId");
 
+    // Check if the journey has finished. If not, we won't check for events that
+    // we know are in the future. That just wouldn't be fair!
     const journeyIsConcluded = vehiclePositions.some(
       (evt) => evt.nextStopId === "EOL" || evt.stop === lastPlannedStop
     );
 
+    // Get a slice of the visited stops from the planned departures.
     const visitedStops = plannedDepartures.slice(0, stopsVisitedCount);
 
+    // Health scores that are scored with a percentage. If data is missing the
+    // percentage is lower. Also includes messages that the validators can add
+    // which can help explain why the score is below 100%. The max value is used
+    // to calculate the percentage and is not returned from this function.
     const healthScores = {
       stops: {health: 0, max: stopsVisitedCount * 4, messages: []},
       positions: {health: 0, max: vehiclePositions.length, messages: []},
     };
 
+    // Function that increments the health points for a specific health check.
     const onIncrementHealth = (which) => (addPoints = 0) => {
-      healthScores[which].health += addPoints;
+      const currentHealth = healthScores[which].health;
+      healthScores[which].health = Math.max(0, currentHealth + addPoints);
     };
 
+    // Function that adds a message for a specific health check.
+    const onAddMessage = (which) => (message) => {
+      healthScores[which].messages.push(message);
+    };
+
+    // Health scores that are scored with a boolean. If any of these are "false", the
+    // data is insufficient and should not be relied upon. They can also be pending if,
+    // for example, the journey is ongoing and hasn't reached a timing stop yet. Pending
+    // checks are not counted in the total health score.
     const checklist = {
       firstStopDeparture: HealthChecklistValues.PENDING,
       lastStopArrival: HealthChecklistValues.PENDING,
@@ -204,38 +224,42 @@ export const useJourneyHealth = (journey) => {
       checklist.timingStopDepartures = HealthChecklistValues.PENDING;
     }
 
+    // Approve or fail a binary health check.
     const onChecklistChange = (which) => (setValue = HealthChecklistValues.PASSED) => {
       checklist[which] = setValue;
     };
 
-    const onAddMessage = (which) => (message) => {
-      healthScores[which].messages.push(message);
-    };
-
+    // Check the health of the vehicle position events stream.
     checkPositionEventsHealth(
       vehiclePositions,
       onIncrementHealth("positions"),
       onAddMessage("positions")
     );
 
+    // Check the health of stop events.
     checkStopEventsHealth(
       stopEvents,
-      visitedStops,
+      journeyIsConcluded ? plannedDepartures : visitedStops,
       onIncrementHealth("stops"),
       onAddMessage("stops")
     );
 
+    // Check that the vehicle has functioning door sensors.
     checkDoorEventsHealth(events, onChecklistChange("doors"));
 
+    // Check the departure from the first stop.
     checkFirstStopDeparture(
       stopEvents,
       plannedDepartures,
       onChecklistChange("firstStopDeparture")
     );
+
+    // Check that the vehicle GPS is working.
     checkGPS(vehiclePositions, onChecklistChange("GPS"));
 
     // Keep these pending until the journey is complete
     if (journeyIsConcluded) {
+      // Check that the last stop arrival event is present.
       checkLastStopArrival(
         stopEvents,
         lastPlannedStop,
@@ -243,6 +267,7 @@ export const useJourneyHealth = (journey) => {
       );
 
       if (typeof checklist.timingStopDepartures !== "undefined") {
+        // Check that important timing stop events are present.
         checkTimingStopDepartures(
           stopEvents,
           plannedDepartures,
@@ -251,6 +276,7 @@ export const useJourneyHealth = (journey) => {
       }
     }
 
+    // Calculate the percentage scores
     const calculatedScores = Object.entries(healthScores).reduce(
       (categories, [name, values]) => {
         const healthScore =
@@ -263,13 +289,15 @@ export const useJourneyHealth = (journey) => {
       {}
     );
 
+    // Get all criteria as a number score. Binary checks give 100 if true or 0 if false.
     const allCriteria = [
       ...Object.values(calculatedScores).map((val) => val.health),
       ...Object.values(checklist)
-        .filter((check) => check !== HealthChecklistValues.PENDING)
+        .filter((check) => check !== HealthChecklistValues.PENDING) // Skip pending states.
         .map((check) => (check === HealthChecklistValues.PASSED ? 100 : 0)),
     ];
 
+    // Calculate the total health of the journey data.
     const totalHealth = allCriteria.some((val) => val === 0)
       ? 0
       : round(
