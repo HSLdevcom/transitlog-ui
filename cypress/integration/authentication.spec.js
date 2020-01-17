130238b208b9b2a79dfd8863e01acc2bea2b6aa0
Daniel Dunderfelt <daniel@developsuperpowers.com>
2020-01-17 09:45:54 +0200

    Update changelog.

--- cypress/integration/authentication.spec.js
+++ cypress/integration/authentication.spec.js
@@ -6,8 +6,6 @@ describe("Authentication smoke tests", () => {
   });
 
   it("Can log in with HSL ID", () => {
-    cy.getCookie("transitlog-session").should("not.exist");
-
     cy.hslLogin();
 
     cy.getTestElement("authenticated-user").should("exist");
