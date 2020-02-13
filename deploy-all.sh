70ce3f259fe240bee0c6bbc45dc5c96407253ac9
Daniel Dunderfelt <daniel@developsuperpowers.com>
2020-02-13 05:21:25 +0200

    Stop building the e2e testing image when running deploy scripts.

--- deploy-all.sh
+++ deploy-all.sh
@@ -7,11 +7,6 @@ ORG=${ORG:-hsldevcom}
 
 for TAG in dev stage production; do
   DOCKER_IMAGE=$ORG/transitlog-ui:${TAG}
-  DOCKER_IMAGE_E2E=$ORG/transitlog-ui:${TAG}-testing
-
   docker build --build-arg BUILD_ENV=${TAG} -t $DOCKER_IMAGE .
-  docker build -f e2e.dockerfile -t $DOCKER_IMAGE_E2E .
-
   docker push $DOCKER_IMAGE
-  docker push $DOCKER_IMAGE_E2E
 done
--- deploy-env.sh
+++ deploy-env.sh
@@ -46,10 +46,6 @@ echo "Building for the $opt ($ENV) environment..."
 
 ORG=${ORG:-hsldevcom}
 DOCKER_IMAGE=${ORG}/transitlog-ui:${ENV}
-DOCKER_IMAGE_E2E=${ORG}/transitlog-ui:${ENV}-testing
 
 docker build --build-arg BUILD_ENV=${ENV} -t ${DOCKER_IMAGE} .
-docker build -f e2e.dockerfile -t ${DOCKER_IMAGE_E2E} .
-
 docker push ${DOCKER_IMAGE}
-docker push ${DOCKER_IMAGE_E2E}
