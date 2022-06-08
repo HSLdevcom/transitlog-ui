3caa97f9554c9845e906becf29976c9f03ab1e60
Anton Jyrkiäinen <anton.jyrkiainen@gmail.com>
2022-06-08 11:02:12 +0300

    revert filtering (#742)

--- src/components/CancellationsList.js
+++ src/components/CancellationsList.js
@@ -29,16 +29,9 @@ const CancellationsList = decorate(
   }) => {
     const validCancellations =
       cancellations && Array.isArray(cancellations) ? cancellations : [];
-    const filteredCancellations = validCancellations.filter((c) => {
-      if (
-        c.cancellationType === "CANCEL_DEPARTURE" ||
-        c.cancellationType === "CANCEL_ENTIRE_DEPARTURE"
-      ) {
-        return c;
-      }
-    });
+
     const cancellationGroups = groupBy(
-      filteredCancellations,
+      validCancellations,
       ({departureDate, journeyStartTime, routeId, direction}) =>
         departureDate + journeyStartTime + routeId + direction
     );
